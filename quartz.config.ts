e28adfe3432f557832aa54061593f72c6f396140
randy <randy_letran@sfu.ca>
2026-03-08 00:56:35 -0800

    update baseUrl in quartz config

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-   baseUrl: "MaxiKolbe.github.io/ProjectStatBook",
+   baseUrl: "maxikolbe.github.io/ProjectStatBook",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
